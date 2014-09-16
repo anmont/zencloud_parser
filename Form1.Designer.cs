510235f19558efbfc2cba1f0063c0c18b5b50a53
anmont <anmont@gmail.com>
2014-09-17 00:58:13 +0800

    initial checkin

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace zencloud_parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParse = new System.Windows.Forms.Button();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.txtOpen = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(317, 62);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 1;
+            this.btnParse.Text = "Process";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // rtbInfo
+            // 
+            this.rtbInfo.Location = new System.Drawing.Point(12, 71);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.Size = new System.Drawing.Size(19, 14);
+            this.rtbInfo.TabIndex = 0;
+            this.rtbInfo.Text = "";
+            this.rtbInfo.Visible = false;
+            // 
+            // txtOpen
+            // 
+            this.txtOpen.Location = new System.Drawing.Point(12, 28);
+            this.txtOpen.Name = "txtOpen";
+            this.txtOpen.Size = new System.Drawing.Size(299, 20);
+            this.txtOpen.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "File To Open";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 91);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOpen);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.rtbInfo);
+            this.Name = "Form1";
+            this.Text = "ZenCloud Parser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+        private System.Windows.Forms.TextBox txtOpen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace zencloud_parser
+{
+    public partial class Form1 : Form
+    {
+        public string[] rawTransactions;
+        public List<string> processedTransactions = new List<string>();
+        public int id_count = 1;
+        public OpenFileDialog openFile = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+                string html = File.ReadAllText(openFile.FileName);
+                prep_file(html);
+                process_transactions();
+                writeTobox();
+                fileSave();
+                Application.Exit();
+
+        }
+
+        private void prep_file(string file)
+        {
+            string replace = "\"user\":{\"_id\"";
+            string replaceWith = "\n\n\"user\":{\"_id\"";
+
+            string newSt = file.Replace(replace, replaceWith);
+
+            rawTransactions = newSt.Split(new string[] { "\n\n" }, StringSplitOptions.None);
+        }
+
+        private void process_transactions()
+        {
+            foreach (string tString in rawTransactions)
+            {
+                int loc = tString.IndexOf("action\\\">") + 9;
+                if (loc > 8)
+                {
+                    int loca = tString.IndexOf("<", loc);
+                    string compare = tString.Substring(loc, loca - loc);
+                    if (compare == "service charge")
+                    {
+                        process_charge(tString);
+                    }
+                    else if (compare == "payout")
+                    {
+                        process_payout(tString);
+                    }
+                    else if (compare == "transfer completed")
+                    {
+                        process_transfer(tString);
+                    }
+                }
+            }
+        }
+
+        private void process_transfer(string transfer)
+        {
+            string type0 = "transfer in";
+            string date0 = "";
+            string time0 = "";
+            string amount0 = "";
+            string device0 = " ";
+
+            //pull date
+            int locD = transfer.IndexOf("atedAt\":\"") + 9;
+            int locaD = transfer.IndexOf("T", locD);
+            date0 = transfer.Substring(locD, locaD - locD);
+            //pull time
+            int locbT = transfer.IndexOf("Z", locaD);
+            time0 = transfer.Substring(locaD + 1, locbT - locaD - 1);
+            //pull amount
+            int loc = transfer.IndexOf("funding for ") + 12;
+            int loca = transfer.IndexOf(" ", loc);
+            amount0 = transfer.Substring(loc, loca - loc);
+            //pull device
+            if (transfer.IndexOf("\"devices\":[]") == -1)
+            {
+                int locDe = transfer.IndexOf("\"name\":\"") + 8;
+                int locaDe = transfer.IndexOf("\"", locDe);
+                device0 = transfer.Substring(locDe, locaDe - locDe);
+            }
+
+
+
+                string full = build_string(type0, date0, time0, amount0, device0);
+
+                processedTransactions.Add(full);
+
+        }
+
+        private void process_payout(string payout)
+        {
+            string type0 = "payout";
+            string date0 = "";
+            string time0 = "";
+            string amount0 = "";
+            string device0 = " ";
+
+            
+            //pull date
+            int locD = payout.IndexOf("atedAt\":\"") + 9;
+            int locaD = payout.IndexOf("T", locD);
+            date0 = payout.Substring(locD, locaD - locD);
+            //pull time
+            int locbT = payout.IndexOf("Z", locaD);
+            time0 = payout.Substring(locaD + 1, locbT - locaD - 1);
+            //pull amount
+            int loc = payout.IndexOf("yout of ") + 8;
+            int loca = payout.IndexOf(" ", loc);
+            amount0 = payout.Substring(loc, loca - loc);
+            //pull device
+            if (payout.IndexOf("\"devices\":[]") == -1)
+            {
+                int locDe = payout.IndexOf("\"name\":\"") + 8;
+                int locaDe = payout.IndexOf("\"", locDe);
+                device0 = payout.Substring(locDe, locaDe - locDe);
+            }
+
+            if (payout.IndexOf(" HashPoints<") == -1)
+            {
+                string full = build_string(type0, date0, time0, amount0, device0);
+
+                processedTransactions.Add(full);
+            }
+        }
+
+        private void process_charge(string charge)
+        {
+            string type0 = "service charge";
+            string date0 = "";
+            string time0 = "";
+            string amount0 = "";
+            string device0 = " ";
+
+            //pull date
+            int locD = charge.IndexOf("atedAt\":\"") + 9;
+            int locaD = charge.IndexOf("T", locD);
+            date0 = charge.Substring(locD, locaD - locD);
+            //pull time
+            int locbT = charge.IndexOf("Z", locaD);
+            time0 = charge.Substring(locaD+1, locbT - locaD -1);
+            //pull amount
+            int loc = charge.IndexOf("ce fee -") + 8;
+            int loca = charge.IndexOf(" ", loc);
+            amount0 = "-" + charge.Substring(loc, loca - loc);
+            //pull device
+            if (charge.IndexOf("\"devices\":[]") == -1)
+            {
+                int locDe = charge.IndexOf("\"name\":\"") + 8;
+                int locaDe = charge.IndexOf("\"", locDe);
+                device0 = charge.Substring(locDe, locaDe - locDe);
+            }
+
+
+            string full = build_string(type0, date0, time0, amount0, device0);
+            processedTransactions.Add(full);
+        }
+
+        private string build_string(string type, string date, string time, string amount, string device)
+        {
+            string stringRet = "";
+
+            stringRet = id_count.ToString() + "," + type + "," + date + "," + time + "," + amount + "," + device;
+            id_count++;
+
+            return stringRet;
+        }
+
+        private void writeTobox()
+        {
+            foreach (string tString in processedTransactions)
+            {
+                rtbInfo.Text = rtbInfo.Text + tString + "\n";
+            }
+        }
+
+        private void fileSave()
+        {
+            
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+            saveFile1.DefaultExt = "*.csv";
+            saveFile1.Filter = "CSV Files|*.csv";
+
+            if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               saveFile1.FileName.Length > 0)
+            {
+                File.WriteAllLines(saveFile1.FileName, processedTransactions);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               openFile.FileName.Length > 0)
+            {
+                txtOpen.Text = openFile.FileName;
+            }
+            if (txtOpen.Text != "")
+            {
+                btnParse.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnParse.Enabled = false;
+        }
+    }
+}
