3a3228e807b4fba21e8b5cb03553ce6b0ec2cfc5
anmont <anmont@gmail.com>
2018-04-30 14:53:24 +0800

    added unrelated sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+if [ ! -f /build/dsyn ]; then 
+	cd /build 
+	mkdir edm_yocto 
+	git config --global user.email "you@email.com" 
+	git config --global user.name "Bob Dole" 
+	cd edm_yocto 
+	/home/bin/repo init -u "https://github.com/TechNexion/edm-yocto-bsp.git" -b krogoth_4.1.y_GA 
+	/home/bin/repo sync -j8 
+	touch /build/dsyn
+fi
